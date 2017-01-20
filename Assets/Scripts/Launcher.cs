3a9d3f05de2776c8f20176949d65eef5f67bcbc7
Richard G Schmidt Jr <richard.g.schmidt.jr@gmail.com>
2017-01-20 05:23:18 -0600

    Built Model and Animated it in Blender, completed import into Unity3d
    Controlled with a 3rd person controller, plan to use this for game test
    scenes for multiplayer testing.

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.Reds.PhotonTutorial
+{
+
+
+    public class Launcher : MonoBehaviour
+    {
+        #region Public Variables
+
+        #endregion
+
+        #region Private Variables
+
+        /// <summary>
+        /// This is the client's version number.  Users are seperated from each other by gameversion.
+        /// </summary>
+        string _gameVersion = "1";
+
+        #endregion
+
+        #region MonoBehaviour Callbacks
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
+        /// </summary>
+        private void Awake()
+        {
+            //**Criticial
+            //We Don't join a lobby, there is no need to join a lobby to get the room list.
+            PhotonNetwork.autoJoinLobby = false;
+
+            //**Critical
+            // This ensures that we can use PhotonNetwork.LoadLevel() on the master client and all clients in the
+            // same room sync their level automatically.
+            PhotonNetwork.automaticallySyncScene = true;
+
+            
+        }
+
+
+
+        /// <summary>
+        /// Mono Method called on GameObject b Unity during initiallization phase.
+        /// </summary>
+        void Start()
+        {
+            Connect();
+        }
+
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Starts the connection process.
+        /// -If already connected, we attempt joining a random room
+        /// -If not yet connected, Connect this application instance to Photon Cloud Network
+        /// </summary>
+        /// 
+
+        public void Connect()
+        {
+            //check if connected or not, join if we are, otherwise initiate connection to server
+            if (PhotonNetwork.connected)
+            {
+                //attempts to join random room
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                PhotonNetwork.ConnectUsingSettings(_gameVersion);
+            }
+
+        }
+
+        #endregion
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
